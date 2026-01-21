89f3d77e81b630249053d7062314d4a746350f11
nzkaksum-marto <martinmiroslavovpetrov@gmail.com>
2026-01-21 15:24:31 +0200

    Adding Class Library

---
+++ ZooShopApp/ZooShopApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZooShopApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
